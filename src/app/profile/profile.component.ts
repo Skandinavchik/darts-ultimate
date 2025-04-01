55ae6fac721df33d0f02511b0e819dcf6edc8d7d
Artem Sokolov <sknd2569@gmail.com>
2025-04-01 17:41:01 +0200

    minor ref

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core'
+import { AuthService } from '../auth/auth.service'
 
 @Component({
   selector: 'app-profile',
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
-
+  authService = inject(AuthService)
+  currentUser = computed(this.authService.currentUser)
 }
