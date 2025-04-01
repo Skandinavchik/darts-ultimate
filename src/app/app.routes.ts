b9ef88e654fff0031b5bd2106ed162db565688a8
Artem Sokolov <sknd2569@gmail.com>
2025-04-01 15:02:04 +0200

    added profile lazy route, auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,17 @@ import { TournamentsComponent } from './tournaments/tournaments.component'
 import { LeaguesComponent } from './leagues/leagues.component'
 import { CounterComponent } from './counter/counter.component'
 import { SignUpComponent } from './auth/sign-up/sign-up.component'
+import { authGuard } from './auth/auth.guard'
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent),
+    canActivate: [authGuard],
   },
   {
     path: 'tournaments',
